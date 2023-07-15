65fb4b1abe54b7de018a2eef024652f4a7f4c3dd
fronceee <70807925+fronceee@users.noreply.github.com>
2023-07-15 17:02:36 +0700

    update shop and supply data and type

--- src/core/contexts/data.ts
+++ src/core/contexts/data.ts
@@ -1,10 +1,12 @@
 import { Supply } from '../types';
 
-export const initialSupply: Supply = [
-    {
-        ingredient: { id: 1, name: 'Flour' },
-        amount: 1000,
-        price: 12,
-        unit: 'g',
-    },
+export const initialShops: Shop[] = [];
+export const initialMarkers: Marker[] = [
+    { shodId: 1, lat: 18.52023, lng: 73.856743 },
+    { shodId: 2, lat: 18.52043, lng: 73.856743 },
+    { shodId: 3, lat: 18.52053, lng: 73.856743 },
+];
+export const shops = [
+    { shopName: 'Cow Grade A', cat: 'meat', rating: 4.5 },
+    { shopName: 'Veggies', cat: 'vegettable', rating: 4.2 },
 ];
