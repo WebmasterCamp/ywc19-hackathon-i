33e82311c3242fad6f4ee2e47e9814b723350b6b
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-15 08:16:18 +0700

    feat: setup blank homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import '$styles/globals.css'
-import { Metadata } from 'next'
-import { Layout } from '$components/Layout'
-import { AppProvider } from '@/core/contexts/appProvider'
+import '$styles/globals.css';
+import { Metadata } from 'next';
+import { Layout } from '$components/Layout';
+import { AppProvider } from '@/core/contexts/appProvider';
 
 export const metadata: Metadata = {
     title: 'ywc',
     description: 'dksnd',
-}
+};
 
 interface RootLayoutProps {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-            <body style={{ margin: 0 }}>
+            <body>
                 <AppProvider>
                     <Layout>{children}</Layout>
                 </AppProvider>
             </body>
         </html>
-    )
+    );
 }
