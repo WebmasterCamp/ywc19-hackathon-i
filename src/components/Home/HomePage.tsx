74b33166bac4154aa07be6556b01f4fd48d27bb4
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-16 02:00:21 +0700

    fix merge req

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,24 +1,15 @@
-'use client';
-import { FC } from 'react';
-<<<<<<< HEAD
-import { Header } from '../Header';
-=======
->>>>>>> 601e12052e10180b1dcdba28c2920b4004271d90
-import { HomeCarousel } from './HomeCarousel';
-import { Slogan } from './Slogan';
-import { HotDeal } from './HotDeal';
+"use client";
+import { FC } from "react";
+import { HomeCarousel } from "./HomeCarousel";
+import { Slogan } from "./Slogan";
+import { HotDeal } from "./HotDeal";
 
 export const HomePage: FC = () => {
     return (
         <>
-<<<<<<< HEAD
-            <Header />
             <HomeCarousel />
             <Slogan />
             <HotDeal />
-=======
-            <HomeCarousel />
->>>>>>> 601e12052e10180b1dcdba28c2920b4004271d90
         </>
     );
 };
--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-export { SupplyTable } from './SupplyTable';
-export { ShopHeader } from './ShopHeader';
-export { HomePage } from './HomePage';
-export { HomeCarousel } from './HomeCarousel';
-export { Slogan } from "./Slogan"
-export { HotDeal } from "./HotDeal"
-=======
 export { HomePage } from "./HomePage";
 export { HomeCarousel } from "./HomeCarousel";
->>>>>>> 601e12052e10180b1dcdba28c2920b4004271d90
+export { Slogan } from "./Slogan";
+export { HotDeal } from "./HotDeal";
