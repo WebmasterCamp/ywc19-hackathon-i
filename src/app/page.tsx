fb5793fe391acb02c25477afdd98e2a626c7544d
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-15 11:36:49 +0700

    feat: basic map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Map } from '@/components/Home/Map';
 import { NextPage } from 'next';
 
 const IndexPage: NextPage = () => {
-    return <p>hello world</p>;
+    return (
+        <>
+            <p className="text-red-700 mb-10">hello world</p>
+            <Map />
+        </>
+    );
 };
 
 export default IndexPage;
