92094eaea3b42af5f5375bf2cfd602f28e597b07
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-15 17:34:36 +0700

    feat: form ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { SupplyTable } from '@/components/Home';
-import { Button } from '@mui';
+import { Form } from '@/components/Home/Form';
+import { Box } from '@mui';
 import { NextPage } from 'next';
 
 const IndexPage: NextPage = () => {
     return (
         <>
             <SupplyTable />
+            <Box sx={{ marginTop: 3 }}>
+                <Form />
+            </Box>
         </>
     );
 };
