41d3c00b54c6252e5227642f6306dc978d62f262
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-16 02:26:15 +0700

    fix: remove x-scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <body
                 style={{
                     margin: 0,
+                    overflowX: "hidden",
                 }}
             >
                 <AppProvider>
