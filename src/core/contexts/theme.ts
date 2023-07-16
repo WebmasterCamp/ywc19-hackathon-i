076440e7ccff5c8415648c422e7bedf3cf4fc50a
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-16 11:33:31 +0700

    fix: font

--- src/core/contexts/theme.ts
+++ src/core/contexts/theme.ts
@@ -1,29 +1,28 @@
 "use client";
 
 import { createTheme } from "@mui/material";
-import { IBM_Plex_Sans_Thai, Roboto } from 'next/font/google';
+import { IBM_Plex_Sans_Thai, Roboto } from "next/font/google";
 
-const iBMPlexSansThai:{} = IBM_Plex_Sans_Thai({
-    weight:['400','500','600'],
-    subsets:["thai"]
-})
+const iBMPlexSansThai = IBM_Plex_Sans_Thai({
+    weight: ["400", "500", "600"],
+    subsets: ["thai"],
+});
 
 const roboto = Roboto({
-    weight:'400',
+    weight: "400",
     preload: false,
-})
-
+});
 
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#FF7009',
-          },
+            main: "#FF7009",
+        },
         secondary: {
-            main: '#f50057',
+            main: "#f50057",
         },
     },
     typography: {
-        fontFamily: iBMPlexSansThai.style.fontFamily
-    }
-})
+        fontFamily: iBMPlexSansThai.style.fontFamily,
+    },
+});
