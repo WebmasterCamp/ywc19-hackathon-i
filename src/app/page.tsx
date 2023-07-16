17e29f40d9c414b0e5caebb9352ed5e8638a8950
fronceee <70807925+fronceee@users.noreply.github.com>
2023-07-16 12:03:36 +0700

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
 import { NextPage } from 'next';
 import { HomePage } from '../components/Home';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '@/core/contexts/theme';
-=======
-import { NextPage } from "next";
-import { HomePage } from "../components/Home";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "@/core/contexts/theme";
-import { IBM_Plex_Sans_Thai } from "next/font/google";
-
-const iBMPlexSansThai = IBM_Plex_Sans_Thai({
-    weight: "400",
-    subsets: ["thai"],
-});
->>>>>>> 6ddc7c4cae714ea7a7ae70631e737faf0b6b04e3
 
 const IndexPage: NextPage = () => {
     return (
